bd8fa5dccd31f91ad3b02223562118ffa5fcb01c
Z User <z@container>
2026-04-01 16:27:46 +0000

    deploy: build production output

---
+++ out/_next/static/NWuFHF6sQ_F4k5tJJ19bS/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/strategic-war-game//_next/:path+",
+        "destination": "/strategic-war-game/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
